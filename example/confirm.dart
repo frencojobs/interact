d92c7ac6d1977291310ecddc2c4b9b0e0551d882
Frenco <hey@frenco.dev>
2020-12-20 17:04:37 +0630

    feat: finish confirm component

--- example/confirm.dart
+++ example/confirm.dart
@@ -2,7 +2,18 @@ import 'dart:io' show stdout;
 import 'package:clyde/clyde.dart' show Confirm;
 
 void main() {
-  final answer = Confirm(prompt: "Does it work?").interact();
+  final x = Confirm(prompt: "Does it work?").interact();
+  stdout.writeln(x ? 'Awesome!' : 'Wait what! Check again.');
 
-  stdout.writeln(answer ? 'yes' : 'no');
+  final y = Confirm(
+    prompt: "Is there a default value?",
+    defaultValue: true,
+  ).interact();
+  stdout.writeln(y ? 'Wonderful!' : 'Really! This is sad.');
+
+  final z = Confirm(
+    prompt: "Do you have to hit enter?",
+    waitForNewLine: true,
+  ).interact();
+  stdout.writeln(z ? 'Magnificent!' : 'Ugh, Why!');
 }
--- lib/src/confirm.dart
+++ lib/src/confirm.dart
@@ -7,7 +7,7 @@ import 'framework/framework.dart';
 import 'theme.dart';
 import 'utils/prompt.dart';
 
-class Confirm extends StatefulWidget<bool> {
+class Confirm extends Widget<bool> {
   final String prompt;
   final bool defaultValue;
   final bool waitForNewLine;
@@ -15,14 +15,14 @@ class Confirm extends StatefulWidget<bool> {
 
   Confirm({
     @required this.prompt,
-    this.defaultValue = true,
+    this.defaultValue,
     this.waitForNewLine = false,
   });
 
   Confirm.withTheme({
     @required this.prompt,
     @required this.theme,
-    this.defaultValue = true,
+    this.defaultValue,
     this.waitForNewLine = false,
   });
 
@@ -31,11 +31,11 @@ class Confirm extends StatefulWidget<bool> {
 }
 
 class _ConfirmState extends State<Confirm> {
-  bool answer = false;
+  bool answer;
 
   @override
-  void initState() {
-    super.initState();
+  void init() {
+    super.init();
     answer = widget.defaultValue;
     context.console.hideCursor();
   }
@@ -58,7 +58,6 @@ class _ConfirmState extends State<Confirm> {
   }
 
   @override
-  @protected
   void render(Context context) {
     if (context.renderCount > 0) {
       context.console.cursorUp();
@@ -73,42 +72,50 @@ class _ConfirmState extends State<Confirm> {
       message: widget.prompt,
       hint: '(y/n)',
     ));
-    line.write(
-      ' ${widget.theme.promptTheme.defaultStyle(answer ? 'yes' : 'no')}',
-    );
+
+    if (answer != null) {
+      line.write(
+        ' ${widget.theme.promptTheme.defaultStyle(answer ? 'yes' : 'no')}',
+      );
+    }
     context.console.writeLine(line.toString());
   }
 
   @override
   bool interact() {
-    var key = context.console.readKey();
+    while (true) {
+      final key = context.readKey();
 
-    while (!_shouldStop(key)) {
-      switch (key.char) {
-        case 'y':
-        case 'Y':
-          setState(() {
-            answer = true;
-          });
-          break;
-        case 'n':
-        case 'N':
-          setState(() {
-            answer = false;
-          });
-          break;
-        default:
-          break;
+      if (key.isControl) {
+        if (key.controlChar == ControlCharacter.enter &&
+            answer != null &&
+            widget.waitForNewLine) {
+          return answer;
+        }
+      } else {
+        switch (key.char) {
+          case 'y':
+          case 'Y':
+            setState(() {
+              answer = true;
+            });
+            if (!widget.waitForNewLine) {
+              return answer;
+            }
+            break;
+          case 'n':
+          case 'N':
+            setState(() {
+              answer = false;
+            });
+            if (!widget.waitForNewLine) {
+              return answer;
+            }
+            break;
+          default:
+            break;
+        }
       }
-      key = context.console.readKey();
     }
-
-    return answer;
-  }
-
-  bool _shouldStop(Key key) {
-    return key.isControl &&
-        (key.controlChar == ControlCharacter.ctrlC ||
-            key.controlChar == ControlCharacter.enter);
   }
 }
